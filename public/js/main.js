83695ce84bfb40f33bc7932b98b25193d85123b6
sam-m-wlsn <sam.m.wlsn@gmail.com>
2019-03-04 15:21:59 -0600

    feat(index): adds js plugin

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+'use strict';
+
+(function(){
+    const responseBox = document.getElementById('response');
+    responseBox.innerHTML = 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.';
+})();
