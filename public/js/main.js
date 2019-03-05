66e7354850ed2974bd237c86e79d04d73e24d9aa
sam-m-wlsn <sam.m.wlsn@gmail.com>
2019-03-04 20:56:52 -0600

    refactor(scripts): extract search shim to SDK

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,60 @@
 'use strict';
 
-(function(){
+const wikiSDK = (function () {
     // configs
     const apiBase = 'https://en.wikipedia.org/w/api.php';
-    const searchBase = [
-        '?action=query',
-        '&origin=*',
-        '&format=json', 
-        '&list=search', 
-        '&srsearch='
-    ].join('');
 
+    function searchURL(query) {
+        return [
+            apiBase,
+            '?action=query',
+            '&origin=*',
+            '&format=json',
+            '&list=search',
+            '&srsearch=',
+            query
+        ].join('');
+    }
+
+    function searchShim(query, callback) {
+        const request = new XMLHttpRequest();
+
+        request.open('GET', searchURL(query), true);
+        request.onload = function () {
+            if (request.status >= 200 && request.status < 400) {
+                const responseJSON = JSON.parse(request.responseText);
+                if('error' in responseJSON) {
+                    callback(false, getErrorMessage(responseJSON.error.code));
+                } else {
+                    callback(true, responseJSON);
+                }
+            } else {
+                callback(false, getErrorMessage());
+            }
+        }
+        request.onerror = function () {
+            callback(false, getErrorMessage());
+        }
+        request.send();
+    }
+
+    function getErrorMessage(responseCode) {
+        switch(responseCode) {
+            case 'nosrsearch':
+                return 'Search Parameter cannot be empty';
+            default:
+                return 'We\re Sorry, an error was encountered while attempting to get your request. Error code: ' + responseCode;
+        }
+    }
+
+    return {
+        search: searchShim
+    };
+})();
+
+(function(){
     // creates method to write content to DOM
-    const write = (function() {
+    const render = (function() {
         const container = document.getElementById('response');
 
         return function(content) {
@@ -20,69 +62,45 @@
         }
     })();
 
-    // vanilla js ajax shim
-    function get(requestedUrl, success, fail) {
-        const request = new XMLHttpRequest();
-
-        request.open('GET', requestedUrl, true);
-        request.onload = function() {
-            if ( request.status >= 200 && request.status < 400) {
-                success(JSON.parse(request.responseText));
-            } else {
-                fail();
-            }
-        }
-        request.onerror = function() {
-            fail();
-        }
-        request.send();
-    }; 
-
-    // takes control of search form
+    // search form functionality
     const form = document.getElementById('wikiSearch');
     form.addEventListener('submit', function(event){
         event.preventDefault();
         event.stopPropagation();
 
         const searchQuery = event.target.elements['search'].value;
-        const apiUrl = [apiBase, searchBase, searchQuery].join('');
-        get(
-            apiUrl, 
-            function(response){
-                if('error' in response) {
-                    if (response.error.code === 'nosrsearch') write('No search query provided');
-                    else write('We\'re sorry, an error was encountered retreiving the requested data.');
-                } else {
-                    const results = response.query.search.map(function(entry){
-                        const item = document.createElement('article');
-                        item.className = 'result';
-                        
-                        const title = document.createElement('h2');
-                        title.className = 'result-title';
-                        title.innerText = entry.title;
+        console.log(searchQuery);
+        
+        wikiSDK.search(searchQuery, function(success, response){
+            const content = success 
+                ? response.query.search.map(createSearchItem).join('') 
+                : response;
+            render(content);
+        });
 
-                        const description = document.createElement('p');
-                        description.className = 'result-description';
-                        description.innerHTML = entry.snippet;
+        function createSearchItem(entry) {
+            const item = document.createElement('article');
+            item.className = 'result';
 
-                        item.appendChild(title);
-                        item.appendChild(description);
+            const title = document.createElement('h2');
+            title.className = 'result-title';
+            title.innerText = entry.title;
 
-                        return item.outerHTML;
-                    });
+            const description = document.createElement('p');
+            description.className = 'result-description';
+            description.innerHTML = entry.snippet;
 
-                    write(results.join(''));
-                }
-            }, function(){
-                write('We\'re sorry, an error was encountered retreiving the requested data.');
-            }
-        );
+            item.appendChild(title);
+            item.appendChild(description);
+
+            return item.outerHTML;
+        }
 
         return false;
     });
     
 
     // default text on page load
-    write('Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
+    render('Lorem ipsum dolor, sit amet consectetur adipisicing elit.');
 
 })();
